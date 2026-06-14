f60659fd8486221ca5aa3bd8c77ab9f11551f7ba
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-06-14 08:51:41 +0600

    Splice

---
+++ Splice.js
@@ -0,0 +1,10 @@
+var arr = [10,"Mafi",3,4,44,232,212,1234]
+arr.slice(1,3)//for show all element in array
+console.log(arr);
+
+// arr.splice(1,4);
+// arr.splice(1)
+/* arr.splice(2,5,"Mafi","Mafi","Mafi","Mafi","Mafi","Mafi","Mafi",
+    "Mafi"
+)
+console.log(arr); */
