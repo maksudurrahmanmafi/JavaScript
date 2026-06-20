ce67ea9bd58f251b40ac22b651f1114d21fced07
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-06-20 20:16:57 +0600

    countdown

--- Projects/countDown.js
+++ Projects/countDown.js
@@ -8,25 +8,24 @@ const time = setInterval(()=>{
         document.getElementById("timer").innerText="Time's Up"
     }
 },1000); */
+alert("This is your time");
 
 let totalSeconds = 3665; // 1 hour 1 minute 5 second
 
 const timer = setInterval(() => {
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
 
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
+  document.getElementById("timer").innerText =
+    `${String(hours).padStart(2, "0")}:` +
+    `${String(minutes).padStart(2, "0")}:` +
+    `${String(seconds).padStart(2, "0")}`;
 
-    document.getElementById("timer").innerText =
-        `${String(hours).padStart(2, '0')}:` +
-        `${String(minutes).padStart(2, '0')}:` +
-        `${String(seconds).padStart(2, '0')}`;
+  totalSeconds--;
 
-    totalSeconds--;
-
-    if (totalSeconds < 0) {
-        clearInterval(timer);
-        document.getElementById("timer").innerText = "00:00:00";
-    }
-
+  if (totalSeconds < 0) {
+    clearInterval(timer);
+    document.getElementById("timer").innerText = "00:00:00";
+  }
 }, 1000);
---
+++ variable.js
@@ -0,0 +1,2 @@
+let a = 5,b='Hello',c=3;
+console.log(a+b+c);
