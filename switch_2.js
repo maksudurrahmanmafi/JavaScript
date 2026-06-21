32ec1cab88a8726683cef11e34352623ac1184f2
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-06-21 21:26:17 +0600

    switch 2

---
+++ switch_2.js
@@ -0,0 +1,13 @@
+<script>
+let text;
+switch (new Date().getDay()) {
+  default:
+    text = "Looking forward to the Weekend";
+    break;
+  case 6:
+    text = "Today is Saturday";
+    break;
+  case 0:
+    text = "Today is Sunday";
+}
+</script>
